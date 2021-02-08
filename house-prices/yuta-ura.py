ebbfdd3e7cce3a5ecbe7d4a33c13d074abee1c15
YutaUra <yuuta3594@outlook.jp>
2021-02-08 19:44:35 +0900

    pathlibを使うように変更

--- house-prices/yuta-ura.py
+++ house-prices/yuta-ura.py
@@ -1,9 +1,15 @@
 # %%
+from pathlib import Path
+
 import pandas as pd
 
 # %%
+data_dir = (Path().parent / 'data').absolute()
 
-df = pd.read_csv('./data/train.csv')
+
+# %%
+
+df = pd.read_csv(data_dir / 'train.csv')
 
 df.head()
 
